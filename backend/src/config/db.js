af6cf7f3b33f20c74067594b8c3085406387d447
godwinsallah16 <godwinsallah16@gmail.com>
2025-07-30 13:09:59 +0000

    Remove backend folder from frontend branch

--- backend/src/config/db.js
+++
@@ -1,5 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/voting_system',
-});
-module.exports = pool;
